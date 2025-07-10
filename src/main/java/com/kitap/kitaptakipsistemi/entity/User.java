b2004b04412f28cb9092291998607fd8658abbc9
sudeacikose <sudeacikose@gmail.com>
2025-07-10 12:01:43 +0300

    User sınıfında hata vardı düzelttim, pom.xml'e eksik şeyleri ekledim

--- src/main/java/com/kitap/kitaptakipsistemi/entity/User.java
+++ src/main/java/com/kitap/kitaptakipsistemi/entity/User.java
@@ -1,4 +1,27 @@
 package com.kitap.kitaptakipsistemi.entity;
+//User tablosu veritabanında kullanıcıları tutucak
+//Her kullanıcının id,username,password bilgisi olucak
+import jakarta.persistence.*;
+import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long    id;
+    @Column(nullable = true)
+    private String username;
+    @Column(nullable=false)
+    private String password;
+
+
 }
