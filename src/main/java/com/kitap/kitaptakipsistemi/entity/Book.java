108ab82387cffcf19a9c48d38d0afb9b80233cc9
sudeacikose <sudeacikose@gmail.com>
2025-07-10 14:38:53 +0300

    Book sınıfına açıklayıcı yorumlar eklendi

--- src/main/java/com/kitap/kitaptakipsistemi/entity/Book.java
+++ src/main/java/com/kitap/kitaptakipsistemi/entity/Book.java
@@ -1,4 +1,30 @@
 package com.kitap.kitaptakipsistemi.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import jakarta.persistence.ManyToOne;
+import lombok.Setter;
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Book {
+    //Her kitap için benzersiz ID oluşması için kullanıldı
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String author;
+    //Kitapların bir kullanıcıya ait olması için kullanıldı
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
 }
