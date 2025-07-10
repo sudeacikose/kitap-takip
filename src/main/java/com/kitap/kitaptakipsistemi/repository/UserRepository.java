8b0881fbf1da1cec79263e3d6691a975ecf1e140
sudeacikose <sudeacikose@gmail.com>
2025-07-10 14:39:12 +0300

    UserRepository'de küçük düzeltmeler yapıldı

--- src/main/java/com/kitap/kitaptakipsistemi/repository/UserRepository.java
+++ src/main/java/com/kitap/kitaptakipsistemi/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.kitap.kitaptakipsistemi.repository;
+//veri tabanındaki kullanıcılar üzerinde işlem yapmak için kullanılır
+//JpaRepository sayesinde hazır metotları kullanabiliyoruz
+import com.kitap.kitaptakipsistemi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class UserRepository {
+import java.util.Optional;
+public interface UserRepository extends JpaRepository<User,Long>{
+
+    Optional<User> findByUsername(String username);
 }
