e034e51994c226daf4702621abd4a2a7c233430c
sudeacikose <sudeacikose@gmail.com>
2025-07-10 14:39:29 +0300

    UserService kodları açıklayıcı hale getirildi

--- src/main/java/com/kitap/kitaptakipsistemi/service/UserService.java
+++ src/main/java/com/kitap/kitaptakipsistemi/service/UserService.java
@@ -1,4 +1,20 @@
 package com.kitap.kitaptakipsistemi.service;
-
+import com.kitap.kitaptakipsistemi.entity.User;
+import com.kitap.kitaptakipsistemi.repository.UserRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+
+    }
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 }
